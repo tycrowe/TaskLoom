6515d35749cf365c8de54cdf911922ec5d0201df
tycrowe <me@tycrowe.com>
2024-02-02 17:42:25 -0500

    Removing .idea

---
+++ cmake-build-debug/src/CMakeFiles/task-loom.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/me/Projects/TaskLoom/src/backend/DatabaseClient.cpp" "src/CMakeFiles/task-loom.dir/backend/DatabaseClient.cpp.obj" "gcc" "src/CMakeFiles/task-loom.dir/backend/DatabaseClient.cpp.obj.d"
+  "C:/Users/me/Projects/TaskLoom/src/cli/TaskLoomInterface.cpp" "src/CMakeFiles/task-loom.dir/cli/TaskLoomInterface.cpp.obj" "gcc" "src/CMakeFiles/task-loom.dir/cli/TaskLoomInterface.cpp.obj.d"
+  "C:/Users/me/Projects/TaskLoom/src/main.cpp" "src/CMakeFiles/task-loom.dir/main.cpp.obj" "gcc" "src/CMakeFiles/task-loom.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/CMakeFiles/task-loom.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for task-loom.
+# This may be replaced when dependencies are built.
